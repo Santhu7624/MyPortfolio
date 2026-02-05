93bce2583c4e2d6410deda1637d6b4920b28be82
santhosh <santhosh_ganji@attsystemsgroup.com>
2026-02-05 11:38:45 +0800

    updates for tech stak page and profile home 2page

--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -8,7 +8,7 @@ import API from "../images/api2.png";
 //import RabbitMq from "../images/RabbitMQ.png";
 //import ActiveMq from "../images/active.jpg";
 import AzDevops from "../images/Azure-Devops.png";
-import profile from "../images/profile/profile2.jpeg";
+import profile from "../images/profile/profile7.jpeg";
 import CV from "../assets/GanjiSanthosh_SeniorSoftwareEngineer.pdf";
 
 const icons = [
@@ -35,8 +35,8 @@ export default function Home2() {
   <motion.div
     key={idx}
     initial={{ y: 0 }}
-    animate={{ y: [0, -10, 0] }}
-    transition={{ duration: 4 + idx, repeat: Infinity }}
+    animate={{ y: [0, -20, 0] }}
+    transition={{ duration: 4 + idx * 0.3, repeat: Infinity }}
     className="absolute z-10 hidden md:block" // 👈 This line hides icons on mobile
     style={{
       left: item.x,
@@ -49,7 +49,7 @@ export default function Home2() {
 
 
       {/* Main Content */}
-      <div className="flex flex-col md:flex-row items-center justify-between w-full px-4 py-2 md:px-20">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full px-4 py-6 md:px-20 md:py-20 min-h-[130%] md:min-h-0">
         {/* Text Panel */}
         <motion.div
           initial={{ opacity: 0, y: 40 }}
@@ -61,7 +61,7 @@ export default function Home2() {
           <h1 className="text-3xl md:text-3xl font-bold text-white">
             <span className="text-cyan-400">SANTHOSH GANJI</span>
           </h1>
-          <p className="text-gray-300 text-2xl font-semibold">Full Stack Developer</p>
+          <p className="text-gray-300 text-2xl font-semibold">Senior Full Stack Engineer</p>
           <p className="text-gray-800 text-lg text-white">I'm a passionate developer with expertise in .NET, React, Angular, AWS, Azure, and cloud-based microservices. I build scalable and modern apps.</p>
           {/* Buttons */}
           <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
@@ -92,14 +92,20 @@ export default function Home2() {
           transition={{ duration: 1.5, delay: 0.5, type: "spring", stiffness: 100 }}
           className="md:w-1/2 mt-10 md:mt-0 flex justify-center relative md:ml-08"
         >
-          <div className="relative group">
+          {/* <div className="relative group">
             <img
               src={profile}
               alt="Profile"
               //style={{ width: profileSize, height: profileSize }}
               //className="w-[280px] md:w-[350px] rounded-full shadow-xl transition-all duration-500 group-hover:scale-105"
               className="w-[200px] md:w-[400px] rounded-full border-0 border-blue-300 shadow-xl transition-all duration-500 group-hover:scale-105 px-8 py-8"
+            /> */}
+            <div className="w-96 h-96 overflow-hidden rounded-full">
+            <img
+              src={profile}
+              className="w-full h-full object-cover object-right translate-x-1/6"
             />
+          
             <div className="absolute -inset-2 bg-cyan-500 rounded-3xl blur-2xl opacity-30 z-[-1]" />
           </div>
         </motion.div>
--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -23,6 +23,7 @@ import {
   FaTools,
  
   FaBoxes,
+  FaPython,
   
 } from "react-icons/fa";
 import ASPNET from "../images/aspnet.jpg";
@@ -31,6 +32,9 @@ import API from "../images/api2.png";
 import RabbitMq from "../images/RabbitMQ.png";
 import ActiveMq from "../images/active.jpg";
 import AzDevops from "../images/Azure-Devops.png";
+import awseks from "../images/aws-eks.png";
+import azureaks from "../images/Kubernetes.png";
+import { img } from "framer-motion/client";
 // import {
 //   SiCsharp,
 //   SiTypescript,
@@ -50,10 +54,8 @@ const techStack = [
     items: [
       { name: "C#", icon: <img src={CSharp} alt="CSHARP" className="w-8 h-8"></img> },
       { name: "JavaScript", icon: <FaJs className="text-yellow-500" /> },
-      { name: "HTML", icon: <FaHtml5 className="text-orange-500" /> },
-      { name: "CSS", icon: <FaCss3Alt className="text-blue-500" /> },
-      { name: "Java", icon: <FaJava className="text-red-500" /> },      
-      
+      { name: "Python", icon: <FaPython className="text-green-600" /> },  
+      { name: "Java", icon: <FaJava className="text-red-500" /> },  
     ],
   },
   {
@@ -65,8 +67,10 @@ const techStack = [
         },
       { name: "React", icon: <FaReact className="text-cyan-500" /> },
       { name: "Angular", icon: <FaAngular className="text-red-500" /> },
-      { name: "Node.js", icon: <FaNode className="text-green-600" /> },      
+          
       { name: "REST API", icon: <img src={API} alt="API" className="w-8 h-8"/> },
+      { name: "HTML", icon: <FaHtml5 className="text-orange-500" /> },
+      { name: "CSS", icon: <FaCss3Alt className="text-blue-500" /> },
       
     ],
   },
@@ -85,6 +89,7 @@ const techStack = [
   items: [
     { name: "AWS", icon: <FaAws className="text-orange-400" /> },
     { name: "Azure", icon: <FaMicrosoft className="text-blue-700" /> },
+    { name: "GCC-GovTech", icon: <FaCloud className="text-blue-700" /> },
   ],
 },
 {
@@ -93,8 +98,8 @@ const techStack = [
     { name: "Docker", icon: <FaDocker className="text-blue-500" /> },
     { name: "Kubernetes", icon: <FaCubes className="text-blue-400" /> },
     { name: "Microservices", icon: <FaBoxes className="text-orange-500" /> },
-    { name: "EKS", icon: <FaCloud className="text-blue-600" /> },
-    { name: "AKS", icon: <FaCloud className="text-indigo-600" /> },
+    { name: "EKS", icon: <img src ={awseks} alt="AWS-EKS" className="w-8 h-8"></img> },
+    { name: "AKS", icon: <img src={azureaks} alt="azure-aks" className="w-8 h-8"></img>},
     { name: "Azure DevOps", icon: <img src={AzDevops} alt="Azure DevOps" className="w-8 h-8"></img> },
     { name: "Atlassian Bamboo", icon: <FaTools className="text-green-700" /> },
     { name: "GitLab", icon: <FaGitlab className="text-orange-600" /> },
@@ -111,26 +116,67 @@ const techStack = [
 
 ];
 
+// export default function Techstack() {
+//   return (
+//     <div className="px-4 py-2 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+//         <h2 className="text-2xl font-bold text-center mb-6 font-semibold text-gray-800 dark:text-gray-300">Tech Stack</h2>
+//       {techStack.map((section, index) => (
+//         <div key={index} className="mb-4">
+//           <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-400">{section.category}</h2>
+//           <div className="flex flex-wrap gap-4">
+//             {section.items.map((item, idx) => (
+//               <div
+//                 key={idx}
+//                 className="flex flex-col items-center w-20 text-center text-sm text-gray-700 dark:text-gray-300"
+//               >
+//                 <div className="text-3xl mb-1">{item.icon}</div>
+//                 <span>{item.name}</span>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
 export default function Techstack() {
   return (
-    <div className="px-4 py-2 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <h2 className="text-2xl font-bold text-center mb-6 font-semibold text-gray-800 dark:text-gray-300">Tech Stack</h2>
-      {techStack.map((section, index) => (
-        <div key={index} className="mb-4">
-          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-400">{section.category}</h2>
-          <div className="flex flex-wrap gap-4">
-            {section.items.map((item, idx) => (
-              <div
-                key={idx}
-                className="flex flex-col items-center w-20 text-center text-sm text-gray-700 dark:text-gray-300"
-              >
-                <div className="text-3xl mb-1">{item.icon}</div>
-                <span>{item.name}</span>
-              </div>
-            ))}
+    <div className="px-6 py-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <h2 className="text-2xl font-bold text-center mb-8 text-gray-800 dark:text-gray-300">
+        Tech Stack
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+        {techStack.map((section, index) => (
+          <div key={index} className="relative flex flex-col px-4">
+             {/* Vertical divider (right side) */}
+          {index !== techStack.length - 1 && (
+            <span className="absolute top-8 right-0 h-[calc(100%-2rem)] w-px bg-gray-300 dark:bg-gray-600"></span>
+          )}
+            {/* Category heading */}
+           <h3 className="text-lg font-semibold mb-2 text-center text-gray-400 dark:text-gray-400">
+            {section.category}
+          </h3>
+
+            <div className="w-12 h-0.5 bg-gray-300 dark:bg-gray-600 mx-auto mb-4"></div>
+
+            {/* Icons */}
+            <div className="flex flex-wrap justify-center gap-4">
+              {section.items.map((item, idx) => (
+                <div
+                  key={idx}
+                  className="flex flex-col items-center w-20 text-center text-sm text-gray-700 dark:text-gray-300"
+                >
+                  <div className="text-3xl mb-1">{item.icon}</div>
+                  {/* <span className="leading-tight">{item.name}</span> */}
+                  <span>{item.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
